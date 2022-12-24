fd46c8f38cefb9c7ebb1e65099395e00ecc59c14
beautifulseoul <1991scarlett@gmail.com>
2022-12-24 21:16:35 +0900

    [FEAT] Oauth2.0 카카오 연동 로그인

--- src/main/java/com/cloneweek/hanghaebnb/controller/UserController.java
+++ src/main/java/com/cloneweek/hanghaebnb/controller/UserController.java
@@ -1,17 +1,18 @@
 package com.cloneweek.hanghaebnb.controller;
 
+import com.cloneweek.hanghaebnb.common.jwt.JwtUtil;
 import com.cloneweek.hanghaebnb.dto.LoginRequestDto;
 import com.cloneweek.hanghaebnb.dto.ResponseMsgDto;
 import com.cloneweek.hanghaebnb.dto.SignupRequestDto;
+import com.cloneweek.hanghaebnb.service.KakaoService;
 import com.cloneweek.hanghaebnb.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -21,6 +22,7 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserService userService;
+    private final KakaoService kakaoService;
 
     // 회원가입
     @PostMapping("/signup")
@@ -50,5 +52,17 @@ public class UserController {
         return ResponseEntity.ok(new ResponseMsgDto(HttpStatus.OK.value(), "사용 가능한 닉네임입니다."));
     }
 
+    @GetMapping("/kakao/callback")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+        // code: 카카오 서버로부터 받은 인가 코드
+        String createToken = kakaoService.kakaoLogin(code, response);
+
+        // Cookie 생성 및 직접 브라우저에 Set
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));      //앞부분이 키값, 뒷부분이 value값
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return "redirect:/api/shop";
+    }
 }
 
