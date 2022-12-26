b76653fdd5ad136dd5ea8d9177f5ee53c784357a
code0613 <vhxmfltm3000@naver.com>
2022-12-26 20:59:35 +0900

    [FEAT]  Valid 기능 추가

--- src/main/java/com/cloneweek/hanghaebnb/dto/SignupRequestDto.java
+++ src/main/java/com/cloneweek/hanghaebnb/dto/SignupRequestDto.java
@@ -3,17 +3,27 @@ package com.cloneweek.hanghaebnb.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+
 
 @Getter
 @NoArgsConstructor
 public class SignupRequestDto {
 
-//    @Pattern(regexp = "[a-z0-9]{4,10}")
+    @Email
+    @Pattern(regexp="^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*..[a-zA-Z]{2,4}$",
+    message = "올바른 형식의 이메일 주소여야 합니다")
+    @NotBlank(message = "이메일은 필수 입력 값입니다")
     private String email;
 
-    @Pattern(regexp = "[a-z0-9A-Z!@#$%^&*]{8,15}")
+
+    @Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,15}",
+        message = "비밀번호는 영문, 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자여야 합니다 합니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다")
     private String password;
 
+    @NotBlank(message = "닉네임은 필수 입력 값입니다")
     private String nickname;
 }
--- src/main/java/com/cloneweek/hanghaebnb/service/RoomService.java
+++ src/main/java/com/cloneweek/hanghaebnb/service/RoomService.java
@@ -68,79 +68,117 @@ public class RoomService {
         return roomResponseDto;
     }
 
-    //숙소 키워드 검색
-    @Transactional(readOnly = true)
-    public List<RoomResponseDto> search(String keyword, Pageable pageable, User user) {
-        // 페이징 처리
-        // 작성날짜 순으로 정렬
-        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
-
-        pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
-
-        Page<Room> roomList = roomRepository.findByTitleContaining(keyword, pageable);
-        List<RoomResponseDto> roomResponseDtos = new ArrayList<>();
-        for(Room room : roomList){
-            roomResponseDtos.add(new RoomResponseDto(room, (checkLike(room.getId(), user))));
-        }
-
-        return roomResponseDtos;
-    }
-
     //숙소 정보 수정
+//    @Transactional
+//    public RoomResponseDto update(Long roomId, RoomRequestDto requestDto, User user, List<MultipartFile> multipartFilelist) throws IOException {
+//
+//        Room room = roomRepository.findById(roomId).orElseThrow(
+//                () -> new CustomException(StatusMsgCode.ROOM_NOT_FOUND)
+//        );
+//        if (room.getUser().getNickname().equals(user.getNickname())) {
+//            room.update(requestDto);
+//        } else {
+//            throw new CustomException(StatusMsgCode.INVALID_USER);
+//        }
+//
+//        if (multipartFilelist != null) {
+//
+//            List<ImageFile> imageFileList = imageFileRepository.findAllByRoom(room);
+//
+//            for (ImageFile File : imageFileList) {
+//                String path = File.getPath();
+//                String filename = path.substring(56);
+//                s3Service.deleteFile(filename);
+//            }
+//
+//            imageFileRepository.deleteAll(imageFileList);
+//
+//            s3Service.upload(multipartFilelist, "static", room, user);
+//        }
+//        return new RoomResponseDto(room, user.getNickname());
+//    }
     @Transactional
-    public RoomResponseDto update(Long roomId, RoomRequestDto requestDto, User user, List<MultipartFile> multipartFilelist) throws IOException {
-
-        Room room = roomRepository.findById(roomId).orElseThrow(
+    public RoomResponseDto update(Long roomId, RoomRequestDto requestDto, User user, List<MultipartFile> multipartFilelist) {
+        Room room = roomRepository.findById(roomId).orElseThrow(                // 글 존재 여부 확인
                 () -> new CustomException(StatusMsgCode.ROOM_NOT_FOUND)
         );
-        if (room.getUser().getNickname().equals(user.getNickname())) {
-            room.update(requestDto);
-        } else {
+
+        if (!room.getUser().getNickname().equals(user.getNickname())) {         // 작성자와 수정자 검증
             throw new CustomException(StatusMsgCode.INVALID_USER);
         }
+        try {
+            room.update(requestDto);
 
-        if (multipartFilelist != null) {
+            if (multipartFilelist != null) {
+                List<ImageFile> imageFileList = imageFileRepository.findAllByRoom(room);
+                for (ImageFile File : imageFileList) {
+                    String path = File.getPath();
+                    String filename = path.substring(56);
+                    s3Service.deleteFile(filename);
+                }
+                imageFileRepository.deleteAll(imageFileList);
 
-            List<ImageFile> imageFileList = imageFileRepository.findAllByRoom(room);
-
-            for (ImageFile File : imageFileList) {
-                String path = File.getPath();
-                String filename = path.substring(56);
-                s3Service.deleteFile(filename);
+                s3Service.upload(multipartFilelist, "static", room, user);
             }
-
-            imageFileRepository.deleteAll(imageFileList);
-
-            s3Service.upload(multipartFilelist, "static", room, user);
+        } catch (IOException e) {
+            throw new CustomException(StatusMsgCode.FILE_UPLOAD_FAILED);
         }
+
         return new RoomResponseDto(room, user.getNickname());
     }
 
 
     //숙소 정보 삭제
+//    @Transactional
+//    public ResponseMsgDto delete(Long roomId, User user) {
+//        Room room = roomRepository.findById(roomId).orElseThrow(
+//                () -> new CustomException(StatusMsgCode.ROOM_NOT_FOUND)
+//        );
+//        if (room.getUser().getNickname().equals(user.getNickname())) {
+//
+//            roomLikeRepository.deleteAllByRoom(room); // 룸에 해당하는 좋아요 삭제
+//
+//            List<ImageFile> imageFileList = imageFileRepository.findAllByRoom(room);
+//            for (ImageFile imageFile : imageFileList) {
+//                String path = imageFile.getPath();
+//                String filename = path.substring(58);
+//                s3Service.deleteFile(filename);
+//            }
+//
+//            imageFileRepository.deleteAllByRoom(room); // 룸에 해당하는 이미지 파일 삭제
+//
+//            roomRepository.deleteById(roomId); // 최종적 룸 삭제
+//        } else {
+//            throw new CustomException(StatusMsgCode.INVALID_USER);
+//        }
+//        return new ResponseMsgDto(StatusMsgCode.DELETE_POST);
+//    }
     @Transactional
-    public ResponseMsgDto delete(Long roomId, User user) {
+    public ResponseMsgDto delete(Long roomId, User user){
         Room room = roomRepository.findById(roomId).orElseThrow(
                 () -> new CustomException(StatusMsgCode.ROOM_NOT_FOUND)
         );
-        if (room.getUser().getNickname().equals(user.getNickname())) {
+        if (!room.getUser().getNickname().equals(user.getNickname())) {
+            throw new CustomException(StatusMsgCode.INVALID_USER);
+        }
+        try {
             roomLikeRepository.deleteAllByRoom(room); // 룸에 해당하는 좋아요 삭제
 
             List<ImageFile> imageFileList = imageFileRepository.findAllByRoom(room);
             for (ImageFile imageFile : imageFileList) {
                 String path = imageFile.getPath();
-                String filename = path.substring(58);
+                String filename = path.substring(56);
                 s3Service.deleteFile(filename);
             }
 
             imageFileRepository.deleteAllByRoom(room); // 룸에 해당하는 이미지 파일 삭제
 
             roomRepository.deleteById(roomId); // 최종적 룸 삭제
-        } else {
-            throw new CustomException(StatusMsgCode.INVALID_USER);
+        } catch (CustomException e) {
+            throw new CustomException(StatusMsgCode.FILE_DELETE_FAILED);
+
         }
         return new ResponseMsgDto(StatusMsgCode.DELETE_POST);
-
     }
 
     //좋아요 여부 확인
