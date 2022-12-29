3f76ecd86e73857eddc9efd19f440586f3bac18d
eunsol-an <dev.eunsol@gmail.com>
2022-12-30 04:24:58 +0900

    [FIX] 닉네임 글자 수 제한 해제
    - 닉네임 NotBlank 어노테이션으로 이메일 중복 체크가 작동하지 않아 삭제

--- src/main/java/com/cloneweek/hanghaebnb/dto/RequestDto/DupliCheckDto.java
+++ src/main/java/com/cloneweek/hanghaebnb/dto/RequestDto/DupliCheckDto.java
@@ -14,7 +14,7 @@ public class DupliCheckDto {
     @Pattern(regexp="^\\w+@\\w+\\.\\w+(\\.\\w+)?$", message = "올바른 형식의 이메일 주소여야 합니다")
     private String email;
 
-    @NotBlank(message = "닉네임은 필수 입력 값입니다")
-    @Size(min = 2, max = 10, message = "최소 2자 이상, 10자 이하를 입력해주세요.")
+//    @NotBlank(message = "닉네임은 필수 입력 값입니다")
+//    @Size(min = 2, max = 10, message = "최소 2자 이상, 10자 이하를 입력해주세요.")
     private String nickname;
 }
